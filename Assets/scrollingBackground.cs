e995292fd525032a8a82b511f517f5d0103934ad
Takanar <dyganar@hotmail.com>
2021-01-22 21:20:08 -0300

    versao 1.0

---
+++ Assets/scrollingBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollingBackground : MonoBehaviour
+{
+    public float bgSpeed;
+    public Renderer bgRend;
+
+    GameObject peixe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        peixe = GameObject.FindWithTag("peixe");
+        if(!peixe){
+
+        } else {
+            bgRend.material.mainTextureOffset += new Vector2(bgSpeed * Time.deltaTime, 0f);
+        }
+    }
+}
---
+++ Assets/tubarao.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tubarao : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject fish;
+    GameObject shark;
+
+    int direcao = 1;
+    void Start()
+    {
+        shark = GameObject.FindWithTag("tubarao");
+        fish = GameObject.FindWithTag("peixe");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        int positionFish =  (int)fish.transform.position.y;
+        int positionShark = (int)shark.transform.position.y;
+
+        if (positionFish - positionShark > 0.5)
+        {
+            direcao = 1;
+            transform.Translate(new Vector2(0, direcao));
+          
+        } else if (positionFish - positionShark == 0.5 || positionFish - positionShark == -0.5)
+        {
+            transform.Translate(new Vector2(0, 0));
+        } else if (positionFish - positionShark < -0.5)
+        {
+            direcao = -1;
+            transform.Translate(new Vector2(0, direcao));
+        }
+    }
+}
