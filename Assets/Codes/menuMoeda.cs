8e42e67e9394556ff783f1bf3879aa31686cf6c3
Takanar <dyganar@hotmail.com>
2021-01-24 14:38:56 -0300

    add placar moedas

---
+++ Assets/Codes/menuMoeda.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class menuMoeda : MonoBehaviour
+{
+
+    public Text ponto;
+    Move peixe;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject obj = GameObject.Find("Peixe");
+        peixe =  obj.GetComponent<Move>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var valor = peixe.totalMoeda;
+        ponto.text = valor.ToString();
+        print(ponto.text);
+    }
+}
